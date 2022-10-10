256d0e38e5dea216f0c18bb4fb0fc7bc706433de
Efrén Ruíz <efren282@outlook.es>
2022-10-10 12:50:39 -0500

    :sparkles: [ADD]: Copying an object with JSON stringify and parse

--- main.js
+++ main.js
@@ -5,17 +5,11 @@ const obj1 = {
     d: "d",
     e: "e",
   },
+  editA() {
+    this.a = "AAAAA";
+  },
 };
 
-const obj2 = {};
-for (prop in obj1) {
-  obj2[prop] = obj1[prop];
-}
-
-const obj3 = Object.assign({}, obj1);
-const obj4 = Object.create(obj1);
-
-console.log(obj1);
+const stringifiedComplexObj = JSON.stringify(obj1);
+const obj2 = JSON.parse(stringifiedComplexObj);
 console.log(obj2);
-console.log(obj3);
-console.log(obj4);
